e69617f444856b0fd82670e09235c726d85e4871
firman <firmantohari123@gmail.com>
2022-12-27 15:01:39 +0700

    menambahkan fitur login dan registrasi

---
+++ kuliah/pertemuan12/detail.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+  header("Location: login.php");
+  exit;
+}
+
+require 'functions.php';
+
+// Ambil id dari URL
+$id = $_GET['id'];
+
+// query data mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+  <style>
+    ul {
+      list-style-type: none;
+      line-height: 20px;
+    }
+  </style>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img src="img/<?= $m['gambar']; ?>" width="100"></li>
+    <li>NRP : <?= $m['nrp']; ?></li>
+    <li>Nama : <?= $m['nama']; ?></li>
+    <li>Email : <?= $m['email']; ?></li>
+    <li>Jurusan : <?= $m['jurusan']; ?></li>
+    <li><a href="ubah.php?id=<?= $m['id']; ?>">Ubah</a> | <a href="hapus.php?id=<?= $m['id']; ?>" onclick="return confirm('Apakah anda yakin ingin menghapus data?');">Hapus</a></li>
+    <li><a href="index.php">Kembali ke Daftar Mahasiswa</a></li>
+  </ul>
+
+</body>
+
+</html>
